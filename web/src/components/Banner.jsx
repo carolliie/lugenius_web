6a3109b05d9ef9420faf1079affe1e75c1b418b2
carolliie <anacvieira1415@gmail.com>
2024-08-09 18:19:36 -0300

    feat: add panel images carrossel

--- web/src/components/Banner.jsx
+++ web/src/components/Banner.jsx
@@ -1,6 +1,13 @@
 import { useSprings, useSpring, animated } from '@react-spring/web';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import vendasImg from '../assets/img/vendas.webp';
 import panelImg from '../assets/img/panel.webp';
+import panelImg2 from '../assets/img/segunda_imagem_home.webp';
+import panelImg3 from '../assets/img/terceira_imagem_home.webp';
+import panelImg4 from '../assets/img/quarta_imagem_home.webp';
+import panelImg5 from '../assets/img/quinta_imagem_home.webp';
 import lojaImg from '../assets/img/loja.webp';
 import logoPretaImg from '../assets/img/logos/logo_preta.svg';
 import iconImg from '../assets/img/icon.svg';
@@ -17,25 +24,45 @@ export default function Banner() {
 
     const images = [
         { src: vendasImg, alt: 'vendas', className: 'lg:h-48 max-[640px]:h-28 max-[640px]:absolute max-[640px]:top-16 max-[640px]:right-0 max-sm:-mx-8 z-10 md:h-24' },
-        { src: panelImg, alt: 'painel', className: 'lg:h-96 max-[640px]:hidden md:h-60' },
+        { src: panelImg, alt: 'painel', className: 'lg:h-96 md:h-60' },
+        { src: panelImg2, alt: 'painel2', className: 'lg:h-96 md:h-60' },
+        { src: panelImg3, alt: 'painel3', className: 'lg:h-96 md:h-60' },
+        { src: panelImg4, alt: 'painel4', className: 'lg:h-96 md:h-60' },
+        { src: panelImg5, alt: 'painel5', className: 'lg:h-96 md:h-60' },
         { src: lojaImg, alt: 'loja', className: 'lg:h-64 max-[640px]:h-96 mx-auto md:h-52' }
     ];
 
     const springs = useSprings(
-        images.length,
-        images.map((_, i) => ({
+        2, // We only need springs for vendasImg and lojaImg
+        [
+            { src: vendasImg, alt: 'vendas', className: 'lg:h-48 max-[640px]:h-28 max-[640px]:absolute max-[640px]:top-16 max-[640px]:right-0 max-sm:-mx-8 z-10 md:h-24' },
+            { src: lojaImg, alt: 'loja', className: 'lg:h-64 max-[640px]:h-96 mx-auto md:h-52' }
+        ].map((image, i) => ({
             from: { transform: 'scale(1)', opacity: 0.8 },
             to: async (next) => {
                 while (true) {
                     await next({ transform: 'scale(1.05)', opacity: 1 });
                     await next({ transform: 'scale(1)', opacity: 1 });
-                    await new Promise(resolve => setTimeout(resolve, (images.length - 1) * 1500));
+                    await new Promise(resolve => setTimeout(resolve, 1500));
                 }
             },
             config: { duration: 1500 },
             delay: i * 1500
         }))
     );
+
+    const carouselSettings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnHover: true,
+        dotsClass: "slick-dots custom-dots", // Adicionando uma classe customizada
+    };
 
     return (
         <animated.div style={props}>
@@ -83,12 +110,27 @@ export default function Banner() {
                         <a href="https://loja.lugenius.com/" className="rounded-full border border-black p-4 px-12 transition duration-300 ease-in-out hover:bg-black hover:text-white max-[640px]:text-sm max-[640px]:p-2 max-[640px]:px-16">Ver demo</a>
                     </div>
                 </div>
-                <div className="flex flex-row items-center lg:p-12 lg:space-x-8 relative max-[640px]:right-4 max-sm:mt-20 max-sm:mb-10 md:space-x-4 md:p-12">
-                    {springs.map((props, i) => (
-                        <animated.img key={i} src={images[i].src} alt={images[i].alt} className={images[i].className} style={props} />
-                    ))}
+            </div>
+            <div className='flex flex-row justify-center items-center mb-32'>
+                <div className="flex flex-row items-center lg:p-12 lg:space-x-8 relative max-[640px]:-right-56 max-sm:-mt-32 max-sm:mb-10 md:space-x-4 md:p-12">
+                    <animated.img src={vendasImg} alt="vendas" className="lg:h-48 max-[640px]:h-24 z-10 md:h-24" style={springs[0]} />
+                </div>
+
+                <div className="w-full lg:w-1/2 md:w-full max-[640px]:hidden my-8">
+                    <Slider {...carouselSettings}>
+                        {[panelImg, panelImg2, panelImg3, panelImg4, panelImg5].map((src, i) => (
+                            <div key={i}>
+                                <img src={src} alt={`painel${i + 1}`} className="w-full h-auto lg:h-96 md:h-60" />
+                            </div>
+                        ))}
+                    </Slider>
+                </div>
+
+                <div className="flex flex-row items-center lg:p-12 lg:space-x-8 relative max-[640px]:right-12 max-sm:mt-20 max-sm:mb-10 md:space-x-4 md:p-12">
+                    <animated.img src={lojaImg} alt="loja" className="lg:h-64 max-[640px]:h-96 mx-auto md:h-52" style={springs[1]} />
                 </div>
             </div>
+
         </animated.div>
     );
 }
